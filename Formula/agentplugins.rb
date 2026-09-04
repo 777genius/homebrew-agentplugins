2a3d5d98b5aa194e9ed7c698c5acadd6c010bcf1
Илия <iliyazelenkog@gmail.com>
2026-09-04 15:17:47 +0300

    chore: update agentplugins to 0.1.45

--- Formula/agentplugins.rb
+++ Formula/agentplugins.rb
@@ -1,26 +1,26 @@
 class Agentplugins < Formula
   desc "Universal installer and lifecycle manager for Agent Plugins 1.0"
   homepage "https://github.com/777genius/universal-agent-plugins"
-  version "0.1.44"
+  version "0.1.45"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.44/agentplugins_0.1.44_darwin_arm64", using: :nounzip
-      sha256 "726c0d810d9b33b782ee2673c81ff2112de5a22fdaca59f590505894434cc896"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.45/agentplugins_0.1.45_darwin_arm64", using: :nounzip
+      sha256 "d6a842da8a96288b4496e89c50ab873b2393b4ce75edb2c6dbf05db3c0726267"
     else
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.44/agentplugins_0.1.44_darwin_amd64", using: :nounzip
-      sha256 "64dd6cd803bf7f4bfa0dfcf3877f2a08a30544b5564c0d301f9c21c902f08101"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.45/agentplugins_0.1.45_darwin_amd64", using: :nounzip
+      sha256 "6f20d20f8560f86555455328a1af7d8a30c5be231208d5f681dfd5e1d606178d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.44/agentplugins_0.1.44_linux_arm64", using: :nounzip
-      sha256 "871f91fcdfa11c96f5e87adee6cb381e129cb5992d23d481ddea469b534143e3"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.45/agentplugins_0.1.45_linux_arm64", using: :nounzip
+      sha256 "f5c4b2dca163c7c2ae39e2ac21b273dce311056777308d5cee2695303c00d051"
     else
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.44/agentplugins_0.1.44_linux_amd64", using: :nounzip
-      sha256 "2b0a5db0cdbc70bdb1b636cc0b90cee6e6ca547ecc2f3880ec904793d7d51d3c"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.45/agentplugins_0.1.45_linux_amd64", using: :nounzip
+      sha256 "464e8aef298be91bf2ec006d7766d15c8a4e58f749a7f3a6adbe9f7cf7854837"
     end
   end
 
