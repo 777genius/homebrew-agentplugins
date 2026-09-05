29ce876925a350de9310cdfaf51ca289bc452ca7
Илия <iliyazelenkog@gmail.com>
2026-09-05 19:26:30 +0300

    chore: update agentplugins to 0.1.49 (#2)

--- Formula/agentplugins.rb
+++ Formula/agentplugins.rb
@@ -1,26 +1,26 @@
 class Agentplugins < Formula
   desc "Universal installer and lifecycle manager for Agent Plugins 1.0"
   homepage "https://github.com/777genius/universal-agent-plugins"
-  version "0.1.46"
+  version "0.1.49"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.46/agentplugins_0.1.46_darwin_arm64", using: :nounzip
-      sha256 "f7d8f00a5cfae6b3ff9f2cdd96be30fb72e2289bc4a0fc3bbd6da99084ecd584"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.49/agentplugins_0.1.49_darwin_arm64", using: :nounzip
+      sha256 "da98641532f5a34138c5ea6ad36dfb00f72ecbe4f6b3b6ae6ab3a61368899d78"
     else
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.46/agentplugins_0.1.46_darwin_amd64", using: :nounzip
-      sha256 "2e7a4a8e7fb2dc39618d2ffd983235666ce2158c01ce2757208514d9ea86a43c"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.49/agentplugins_0.1.49_darwin_amd64", using: :nounzip
+      sha256 "41fef3a91af1a62482c76b2beb512c1a2a71fff9b4346eaf08199fc8f86f07bc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.46/agentplugins_0.1.46_linux_arm64", using: :nounzip
-      sha256 "5be8e4c882d8a32c294af80154bbf99a4da92c9b3aebf8b65b0a3739d68e5eee"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.49/agentplugins_0.1.49_linux_arm64", using: :nounzip
+      sha256 "b31b88efbab6c4057ef353669826d9841a45eab583e1c8662b281b2eec60f9dc"
     else
-      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.46/agentplugins_0.1.46_linux_amd64", using: :nounzip
-      sha256 "8d5c9f079bf76e479359a0e1f3464a35d68d756197f576347ef54a0d06168f70"
+      url "https://github.com/777genius/universal-agent-plugins/releases/download/agentplugins-v0.1.49/agentplugins_0.1.49_linux_amd64", using: :nounzip
+      sha256 "aeff52fce6e96574c34f0feea19c5329f0bb274ea5d4d55eb13dc49b4caa6edf"
     end
   end
 
